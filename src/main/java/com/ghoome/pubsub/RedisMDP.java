dd8eb62917d06061c7033b7cc672abca09dc5d6b
hovey727 <hovey727@126.com>
2012-10-12 17:05:27 +0800

    remove copyright

--- src/main/java/com/ghoome/pubsub/RedisMDP.java
+++ src/main/java/com/ghoome/pubsub/RedisMDP.java
@@ -1,12 +1,5 @@
-/**
- * ZTE ALL RIGHT RESERVED
- */
 package com.ghoome.pubsub;
 
-/**
- * @author <a href="mailto:li.wei158@zte.com.cn">liwei</a>
- * @since 2012-10-12 下午4:48:34
- */
 public class RedisMDP {
 	
 	public void handleMessage(String message) {
